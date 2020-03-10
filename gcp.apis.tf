aa6dcdd2a356467e621d574e1074da882dc02a83
Patrick Lee Scott <pat@patscott.io>
2020-03-09 23:34:35 -0400

    feat: enable APIs

---
+++ gcp.apis.tf
@@ -0,0 +1,40 @@
+resource "google_project_service" "cloudresourcemanager-api" {
+  project            = var.gcp_project
+  service            = "cloudresourcemanager.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "compute-api" {
+  project            = var.gcp_project
+  service            = "compute.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "iam-api" {
+  project            = var.gcp_project
+  service            = "iam.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "cloudbuild-api" {
+  project            = var.gcp_project
+  service            = "cloudbuild.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "containerregistry-api" {
+  project            = var.gcp_project
+  service            = "containerregistry.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "containeranalysis-api" {
+  project            = var.gcp_project
+  service            = "containeranalysis.googleapis.com"
+  disable_on_destroy = false
+}
+resource "google_project_service" "cloudkms-api" {
+  project            = var.gcp_project
+  service            = "cloudkms.googleapis.com"
+  disable_on_destroy = false
+}
