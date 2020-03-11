df77a6bd45e2835eb070ae766ab0fc6e290aa1f0
Patrick Lee Scott <pat@patscott.io>
2020-03-11 19:27:14 -0400

    fix: machine type upgrade

--- cluster.tf
+++ cluster.tf
@@ -37,7 +37,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
 
     node_config {
         preemptible  = true
-        machine_type = "n1-standard-1"
+        machine_type = "n1-standard-2"
         disk_size_gb = 100
 
         metadata = {
